5dabbde4de057e2138de074e47a610d8fe922ba5
Iwan Ryžij <teum3048@yandex.ru>
2024-11-26 05:59:12 +0300

    Add code coverage setup for target

--- cmake/modules/CodeCoverage.cmake
+++ cmake/modules/CodeCoverage.cmake
@@ -13,10 +13,13 @@ endfunction()
 
 function(setup_target_for_coverage NAME EXECUTABLE)
   add_custom_target(${NAME}
+    COMMAND ${CMAKE_COMMAND} --build . --target ${EXECUTABLE}
+    COMMAND lcov --capture --initial --directory . --output-file coverage.base --ignore-errors=unused,empty,inconsistent,mismatch --rc branch_coverage=1
     COMMAND ${EXECUTABLE}
-    COMMAND lcov --capture --directory . --output-file coverage.info --ignore-errors=inconsistent --rc branch_coverage=1 
-    COMMAND lcov --output-file coverage_filtered.info --remove coverage.info "/usr/*" "include/*" "src/gtest*" "*/tests/*" --ignore-errors=unused,empty --rc branch_coverage=1 
-    COMMAND genhtml coverage_filtered.info --output-directory coverage_report --rc branch_coverage=1 
+    COMMAND lcov --capture --directory . --output-file coverage.info --ignore-errors=unused,empty,inconsistent,mismatch --rc branch_coverage=1
+    COMMAND lcov --add-tracefile coverage.base --add-tracefile coverage.info --output-file coverage.total --ignore-errors=unused,empty,inconsistent,mismatch --rc branch_coverage=1
+    COMMAND lcov --output-file coverage_filtered.info --remove coverage.total "/usr/*" "include/*" "src/gtest*" "src/gmock*" "*/tests/*" --ignore-errors=unused,empty,inconsistent,mismatch --rc branch_coverage=1 
+    COMMAND genhtml coverage_filtered.info --output-directory coverage_report --ignore-errors=unused,empty,inconsistent,mismatch --rc branch_coverage=1 
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     COMMENT "Генерация отчёта покрытия кода"
     VERBATIM
---
+++ src/mul/mul.cpp
@@ -0,0 +1,8 @@
+#include "mul.h"
+
+int mul(int a, int b) {
+  if (a == 1) {
+    return 1;
+  }
+  return a * b;
+}
